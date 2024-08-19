2a027f1d73b8dc5d90c56c924ad1005aee0d94e9
Christopher V <ChrisRVill@gmail.com>
2024-08-18 21:35:26 -0700

    created funcs

--- cmd/main.go
+++ cmd/main.go
@@ -60,6 +60,27 @@ func listNotes() {
 	fmt.Println("Here are your notes:")
 }
 
+func viewNote(path string) {
+	fmt.Println("Viewing note...")
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println("Error opening file: ", err)
+		return
+	}
+	defer file.Close()
+	data := make([]byte, 100)
+	for {
+		n, err := file.Read(data)
+		if err != nil {
+			fmt.Println("Error reading file: ", err)
+			return
+		}
+		fmt.Println(string(data[:n]))
+		if n == 0 {
+			break
+		}
+	}
+}
 func editNote() {
 	fmt.Println("Enter the title of the note you would like to edit: ")
 }
